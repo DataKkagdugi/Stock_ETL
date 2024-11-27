6e7af6a3d2df7a1e4e44074071b8ba395d5b935c
Stella Kim <165534849+estellahjkim@users.noreply.github.com>
2024-11-27 14:10:55 +0900

    Update project3_fgi_dag.py
    change text slicing to json load

--- fgi/project3_fgi_dag.py
+++ fgi/project3_fgi_dag.py
@@ -4,13 +4,19 @@ from airflow.models import Variable
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+
+import json
+from datetime import datetime
+import psycopg2
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
-
-from datetime import datetime
-import psycopg2
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 def get_Redshift_connection(autocommit=True):
     hook = PostgresHook(postgres_conn_id='project_redshift_db')
@@ -23,33 +29,48 @@ def Get_today_fgi():
     BASE_URL = "https://production.dataviz.cnn.io/index/fearandgreed/graphdata"
 
     fgi_data = []
-    driver = webdriver.Chrome(options=chrome_options)
-    driver.get(BASE_URL)
+    remote_webdriver = 'remote_chromedriver'
+    with webdriver.Remote(f'{remote_webdriver}:4444/wd/hub', options=chrome_options) as driver:
+        driver.get(BASE_URL)
     # 페이지 소스를 가져옵니다
-    data= driver.page_source[118:191]
-    fgi = data[11:25]
-    rating = data[-34:-20]
+        string_data= driver.page_source
+        json_data = json.loads(string_data)
+        today_data = json_data.get("fear_and_greed")
+        
+        fgi = today_data["score"]
+        rating = today_data["rating"]
 
-    #transform rating and types
-    index = rating.index("\"")
-    rating = rating[:index]
-    print(rating)
+        #rating 변환
+        rating = rating.upper()
+        print(rating)
 
-    #add date info
-    now = datetime.now().date()
-    print(now)
+        #fgi 반올림
+        fgi = int(float(fgi))
+        #add date info
+        now = datetime.now().date()
+        print(now)
+        now = now.isoformat()
 
-    fgi_data.append([now, fgi, rating])
-    return fgi_data
+        fgi_data.append([now, fgi, rating])
+        return fgi_data
 
-def Transform_and_Load():
-    # analytics 스키마 적재 Task
+def Transform_and_Load(**context):
+    # raw_data 스키마 적재 Task
+
+    ti = context['ti']
+    fgi_data = ti.xcom_pull(task_ids='Get_today_fgi')
+
     cur = get_Redshift_connection()
     try:
         cur.execute("BEGIN;")
         date, index, rating = fgi_data[0]
         print(date, index, rating)
-        sql = f"INSERT INTO {schema}.{table} VALUES ('{date}', '{index}', '{rating}')"
+        schema = context['params']['schema']
+        table = context['params']['table']
+        sql = f"""
+        DELETE FROM {schema}.{table} WHERE date = '{date}';
+        INSERT INTO {schema}.{table} VALUES ('{date}', '{index}', '{rating}');
+"""
         cur.execute(sql)
         cur.execute("COMMIT;")   # cur.execute("END;") 
     except (Exception, psycopg2.DatabaseError) as error:
@@ -60,18 +81,20 @@ dag = DAG(
     dag_id = 'project3_fgi',
     start_date = datetime(2023,11,22),
     catchup=False,
-    schedule = '0 8 * * *')
+    schedule = '0 20 * * *')
 
 Get_today_fgi = PythonOperator(
     task_id = 'Get_today_fgi',
     #python_callable param points to the function you want to run 
     python_callable = Get_today_fgi,
+    provide_context=True,
     #dag param points to the DAG that this task is a part of
     dag = dag)
 
 Transform_and_Load = PythonOperator(
     task_id = 'Transform_and_Load',
     python_callable = Transform_and_Load,
+    provide_context=True,
     params = {
         'schema': 'raw_data',
         'table': 'fear_and_greed_index'
